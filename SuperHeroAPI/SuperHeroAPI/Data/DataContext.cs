57607d2fb67b384c153bbf983cb13835b37e9f8e
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-06-28 13:51:59 +0200

    Build DataBase

---
+++ SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Data
+{
+	public class DataContext : DbContext
+	{
+		public DbSet<SuperHero> SuperHeroes { get; set; }
+
+		public DataContext(DbContextOptions<DataContext> options)
+			:base(options)
+		{
+			
+		}
+	}
+}
