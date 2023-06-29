6fcc445c4ea321268816ba2878e1077607ef0f6d
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-06-29 20:18:57 +0200

    CRUD in app component

--- SuperHero.UI/src/app/app.component.ts
+++ SuperHero.UI/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { SuperHero } from './models/super-hero';
+import { SuperHeroService } from './services/super-hero.service';
 
 @Component({
   selector: 'app-root',
@@ -9,27 +11,35 @@ import { SuperHero } from './models/super-hero';
 export class AppComponent {
   title = 'SuperHero.UI';
   public heroes: SuperHero[] = [];
+  public heroToEdit?: SuperHero;
 
-  private getHeroes(): SuperHero[] {
-    const hero1 = {
-      name: 'Amazing Spider Man',
-      firstName: 'Peter',
-      lastName: 'Parker',
-      place: 'New York City',
-    };
-
-    const hero2 = {
-      name: 'Iron Man',
-      firstName: 'Tony',
-      lastName: 'Stark',
-      place: 'Malibu',
-    };
-    
-    this.heroes.push(hero1, hero2);
-    return this.heroes;
-  }
+  constructor(private superHeroService: SuperHeroService) {}
 
   ngOnInit(): void {
-    this.getHeroes()
+    this.superHeroService.getHeroes()
+        .subscribe( (heroes: SuperHero[]) => this.heroes = heroes);
+  }
+
+  public onInitNewHero = (): void => {
+    this.heroToEdit = new SuperHero();
+  }
+
+  public onEdit = (hero: SuperHero): void => {
+    this.heroToEdit = hero;
+  }
+  
+  public  onCreateNewHero = (newHero: SuperHero): void => {
+    this.superHeroService.createNewHero(newHero)
+      .subscribe( (hero: SuperHero) => this.heroes.push(hero) );
+  }
+
+  public onUpdateHero = (hero: SuperHero): void => {
+    this.superHeroService.updateHero(hero)
+      .subscribe();
+  }
+
+  public onDeleteHero = (hero: SuperHero): void => {
+    this.superHeroService.deleteHero(hero)
+      .subscribe( () => this.heroes = this.heroes.filter(sh => sh.id !== hero.id) );
   }
 }
--- SuperHero.UI/src/app/app.module.ts
+++ SuperHero.UI/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
@@ -10,7 +12,9 @@ import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
     EditHeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
